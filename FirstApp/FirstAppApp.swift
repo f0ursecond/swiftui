772568e7782feeadd8ae354bc5228eee581e6d43
f0ursecond <2ez4second@gmail.com>
2024-05-11 11:16:28 +0700

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Alif Zulfanur on 11/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
