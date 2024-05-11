86b285572022c012a521f9e7b5767433dbc15c26
f0ursecond <2ez4second@gmail.com>
2024-05-11 15:08:59 +0700

    init project

---
+++ FirstApp/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  FirstApp
+//
+//  Created by Alif Zulfanur on 11/05/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
