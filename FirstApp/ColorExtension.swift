c1d9d763c14a8f77fa29e431097c806a5487e612
f0ursecond <2ez4second@gmail.com>
2024-05-21 22:33:48 +0700

    HomeView

---
+++ FirstApp/ColorExtension.swift
@@ -0,0 +1,8 @@
+//
+//  ColorExtension.swift
+//  FirstApp
+//
+//  Created by Alif Zulfanur on 19/05/24.
+//
+
+import Foundation
---
+++ FirstApp/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  FirstApp
+//
+//  Created by Alif Zulfanur on 19/05/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
